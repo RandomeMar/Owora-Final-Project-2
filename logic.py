68224d022523517e1ce985fc901c858ef31e1639
RandomeMar <151096223+RandomeMar@users.noreply.github.com>
2023-12-07 21:04:37 -0600

    Added proper documentation

--- logic.py
+++ logic.py
@@ -5,31 +5,36 @@ import csv
 
 
 class Logic(QMainWindow, Ui_MainWindow):
-    def __init__(self):
+    """
+    Method to set up window, 2d-lists, variables, and button functionality
+    :return: None
+    """
+
+    def __init__(self) -> None:
         super().__init__()
         self.setupUi(self)
 
-        # these are not the defaults. Defaults are 10, 10, and 13
-        self.rows = 1
-        self.cols = 1
-        self.bombs = 1
-        self.flags = 1
-        self.board = [[]]
-        self.board_nums = []
-        self.board_flags = []
+        self.rows: int = 1
+        self.cols: int = 1
+        self.bombs: int = 1
+        self.flags: int = 1
+        self.board: list = [[]]
+        self.board_nums: list = []
+        self.board_flags: list = []
         self.set_button(0, 0)
+
         self.generate_board()
         self.button_gen.clicked.connect(lambda: self.generate_board())
-        self.flag_icon = QtGui.QIcon('Flag.png')
+        self.flag_icon: QtGui.QIcon = QtGui.QIcon('Flag.png')
 
-
-
-
-
-    def set_button(self, row, col):
-        # This shit creates a button widget at space (row, col) in the grid layout. Also saves button to .board 2d
-        # list for future fuckery
-        self.button = QtWidgets.QPushButton(parent=self.frame_game)
+    def set_button(self, row: int, col: int) -> None:
+        """
+        Method for creating a push button in a specific row and column in the grid layout
+        :param row: row that the button will be created in
+        :param col: column that the button will be created in
+        :return: None
+        """
+        self.button: QtWidgets.QPushButton = QtWidgets.QPushButton(parent=self.frame_game)
         self.button.setMaximumSize(QtCore.QSize(40, 40))
         self.button.setMinimumSize(QtCore.QSize(40, 40))
         self.button.setObjectName(f"button{row}_{col}")
@@ -38,46 +43,55 @@ class Logic(QMainWindow, Ui_MainWindow):
         self.board[row].append(self.button)
         self.button.clicked.connect(lambda: self.button_pressed(self.board[row][col]))
 
-
-    def button_pressed(self, space):
-        # This shit is retarded
+    def button_pressed(self, space: QtWidgets.QPushButton) -> None:
+        """
+        Method for deciding which method to call when a space is clicked on the board
+        :param space: space on the board being manipulated
+        :return: None
+        """
         if self.radio_flag.isChecked():
             self.flag(space)
         elif self.radio_click.isChecked():
             self.sweep(space)
 
-
-    def sweep(self, space):
+    def sweep(self, space: QtWidgets.QPushButton) -> None:
         """
-        Todo: If a space is clicked, and it's not a bomb, disable it with .setEnabled(False) and change its text to the number in board_nums.
-
-        Todo: If it is a bomb, disable all spaces and change the bomb spaces text to an X. I'll find a bomb image later.
-
-        Todo: If a space is flagged, do nothing
+        Method for when a space is clicked
+        :param space: space on the board being manipulated
+        :return: None
         """
 
         row, col = self.get_button_position(space)
+        row: int
+        col: int
 
         if self.board_flags[row][col] == 'F':
             return
-        
+
         if self.board_nums[row][col] == 'x':
             self.lose()
         else:
             self.flood_fill(row, col)
             self.check_win()
-    
-    def get_button_position(self, button):
-        pos = self.gridLayout.getItemPosition(self.gridLayout.indexOf(button))
+
+    def get_button_position(self, button: QtWidgets.QPushButton) -> tuple[int, int]:
+        """
+        Method for returning the row and column of a button on the grid layout
+        :param button: Button whose coordinates will be gathered
+        :return: a tuple containing the row and column values
+        """
+        pos: list = self.gridLayout.getItemPosition(self.gridLayout.indexOf(button))
         return pos[0], pos[1]
 
-
-
-    def flag(self, space):
+    def flag(self, space: QtWidgets.QPushButton) -> None:
         """
-        Todo: Make it so you can flag and un-flag a space. For now just change the space text to 'F'. I'll get a flag image later
+        Method for when a space is flagged
+        :param space: space on the board being manipulated
+        :return: None
         """
         row, col = self.get_button_position(space)
+        row: int
+        col: int
         if self.board_flags[row][col] == '' and self.flags > 0:
             space.setIcon(self.flag_icon)
             self.board_flags[row][col] = 'F'
@@ -88,15 +102,15 @@ class Logic(QMainWindow, Ui_MainWindow):
             self.flags += 1
         self.lcd_flag.setProperty("intValue", self.flags)
 
-
-
-    def generate_board(self):
-        # This shit uses temp vars to see if the inputs are ints that are > 1. If they are it clears the board list,
-        # destroys all the button widgets in the frame_game frame, and creates a new board with .set_button()
+    def generate_board(self) -> None:
+        """
+        Method for generating the game board and deleting an old game board
+        :return: None
+        """
         try:
-            temp_r = int(self.input_row.text())
-            temp_c = int(self.input_col.text())
-            temp_b = int(self.input_bomb.text())
+            temp_r: int = int(self.input_row.text())
+            temp_c: int = int(self.input_col.text())
+            temp_b: int = int(self.input_bomb.text())
             if temp_r < 1 or temp_c < 1 or temp_b < 1 or temp_b > temp_r * temp_c:
                 raise TypeError
 
@@ -123,7 +137,6 @@ class Logic(QMainWindow, Ui_MainWindow):
                     self.board_nums[row].append(0)
                     self.board_flags[row].append('')
 
-            # This shit is bomb creation
             for i in range(self.bombs):
                 rand_row = randint(0, self.rows - 1)
                 rand_col = randint(0, self.cols - 1)
@@ -138,46 +151,48 @@ class Logic(QMainWindow, Ui_MainWindow):
             self.label_main.setText("Input rows, columns, and bombs to generate a new game")
             self.generate_nums()
 
-
         except ValueError:
             self.label_main.setText('Please input only integers')
 
         except TypeError:
             self.label_main.setText('Make sure all values are positive and that there are more spaces than bombs')
 
-
-    def generate_nums(self):
+    def generate_nums(self) -> None:
         """
-        Todo: This shit should generate the numbers. Make a double for loop to cycle through every space in board_nums.
-        Should look like this:          x 0 x
-                                        0 # 0 = 3
-                                        x 0 0
-        If the space in the middle is an x then just continue.
+        Method for assigning number of bombs nearby for all spaces on the board
+        :return: None
         """
         for row in range(self.rows):
             for col in range(self.cols):
                 if self.board_nums[row][col] != 'x':
-                    num_bombs = self.count_neighboring_bombs(row, col)
+                    num_bombs: int = self.count_neighboring_bombs(row, col)
                     self.board_nums[row][col] = num_bombs
 
-
-
-    def count_neighboring_bombs(self, row, col):
-    #THis shit was toxic bitch
-        bomb_count = 0
+    def count_neighboring_bombs(self, row, col) -> int:
+        """
+        Method for evaluating how many bombs are nearby
+        :param row: row of space being evaluated
+        :param col: column of space being evaluated
+        :return: int measuring amount of bombs nearby
+        """
+        bomb_count: int = 0
 
         for i in range(-1, 2):
             for j in range(-1, 2):
                 new_row, new_col = row + i, col + j
-
-                #Checks the neighboring space to see if it's within the board boundaries
                 if 0 <= new_row < self.rows and 0 <= new_col < self.cols:
                     if self.board_nums[new_row][new_col] == 'x':
                         bomb_count += 1
 
         return bomb_count
 
-    def flood_fill(self, row, col):
+    def flood_fill(self, row, col) -> None:
+        """
+        Recursive method for filling in spaces with bomb count of zero
+        :param row: row of central space for floodfill
+        :param col: column of central space for floodfill
+        :return: None
+        """
         try:
             if row < 0 or col < 0:
                 raise IndexError
@@ -185,7 +200,7 @@ class Logic(QMainWindow, Ui_MainWindow):
                 return
             if self.board_flags[row][col] == 'F':
                 return
-            num = self.board_nums[row][col]
+            num: int = self.board_nums[row][col]
             self.board[row][col].setEnabled(False)
             if num > 0:
                 self.board[row][col].setText(str(num))
@@ -202,14 +217,17 @@ class Logic(QMainWindow, Ui_MainWindow):
         except IndexError:
             return
 
-    def check_win(self):
+    def check_win(self) -> None:
+        """
+        Method for checking if the player has won
+        :return: None
+        """
         for row in range(len(self.board)):
             for col in range(len(self.board[0])):
                 if self.board_nums[row][col] == 'x':
                     continue
                 if self.board[row][col].isEnabled():
                     return
-        # Put win condition
         for row in self.board:
             for button in row:
                 button.setEnabled(False)
@@ -220,11 +238,14 @@ class Logic(QMainWindow, Ui_MainWindow):
                     self.board[row][col].setIcon(QtGui.QIcon('Slightly Smiling Face Emoji.png'))
         self.label_main.setText("You win! Press generate to play again")
         with open("win_record.csv", 'a', newline='') as file:
-            csv_r = csv.writer(file)
-            csv_r.writerow([f'{self.rows}x{self.cols}', self.bombs, "Win"])
+            csv_w: csv.writer = csv.writer(file)
+            csv_w.writerow([f'{self.rows}x{self.cols}', self.bombs, "Win"])
 
-
-    def lose(self):
+    def lose(self) -> None:
+        """
+        Method for displaying the loss state of the game
+        :return: None
+        """
         for row in self.board:
             for button in row:
                 button.setEnabled(False)
@@ -236,6 +257,5 @@ class Logic(QMainWindow, Ui_MainWindow):
         self.label_face.setPixmap(QtGui.QPixmap("Dizzy Face Emoji.png"))
         self.label_main.setText("You lose! Press generate to play again")
         with open("win_record.csv", 'a', newline='') as file:
-            csv_r = csv.writer(file)
-            csv_r.writerow([f'{self.rows}x{self.cols}', self.bombs, "Loss"])
-
+            csv_w: csv.writer = csv.writer(file)
+            csv_w.writerow([f'{self.rows}x{self.cols}', self.bombs, "Loss"])
